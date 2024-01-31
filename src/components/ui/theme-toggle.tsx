3ccce89577988122c8125c4e399136f32999b425
aBgAmeuR <a.josset@yahoo.com>
2024-01-31 22:35:23 +0100

    setup projet

---
+++ src/components/ui/theme-toggle.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { MoonIcon, SunIcon } from "@heroicons/react/solid";
+import { Button, Icon } from "@tremor/react";
+import { useTheme } from "next-themes";
+
+type Props = {
+  variant?: "primary" | "secondary"
+}
+
+
+export function ThemeToggle({ variant = "secondary" }: Props) {
+  const { setTheme, theme } = useTheme()
+
+  return (
+    <Button
+      variant={variant}
+      size="xs"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className="size-[34px]"
+    >
+      <Icon icon={SunIcon} className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" color={variant === "primary" ? "white" : "blue"} />
+      <Icon icon={MoonIcon} className="absolute -translate-x-8 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" color={variant === "primary" ? "white" : "blue"} />
+    </Button>
+  )
+}
