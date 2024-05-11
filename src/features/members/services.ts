eac0fd9357b596bbab0757443c542561b9922f3e
aBgAmeuR <a.josset@yahoo.com>
2024-05-11 22:20:32 +0200

    feat: rework project members page: change table (#18)

--- src/features/members/services.ts
+++ src/features/members/services.ts
@@ -1,6 +1,21 @@
 import { getSelectedProject } from '../projects';
 
+import { getServerSession } from '@/lib/auth';
 import prisma from '@/lib/prisma';
+
+export const getRoleOfCurrentUser = async () => {
+  const session = await getServerSession();
+  if (!session || !session.user.selectProject) return null;
+
+  const res = await prisma.projectMember.findFirst({
+    where: {
+      projectId: session.user.selectProject.id,
+      userId: session.user.id,
+    },
+  });
+
+  return res?.role;
+};
 
 export const getProjectMembers = async () => {
   const selectedProject = await getSelectedProject();
@@ -18,30 +33,69 @@ export const getProjectMembers = async () => {
   return members;
 };
 
-export const getProjectRoles = async () => {
-  const selectedProject = await getSelectedProject();
-  if (!selectedProject) return null;
-
-  const roles = [
-    { role: 'ADMIN' },
-    { role: 'DEVELOPPER' },
-    { role: 'TESTER' },
-    { role: 'VISITOR' },
-  ];
-
-  return roles;
-};
-
 export const changeRoleOfMember = async (memberId: string, role: string) => {
   const selectedProject = await getSelectedProject();
   if (!selectedProject) return null;
 
+  const currentUserRole = await getRoleOfCurrentUser();
+  if (!currentUserRole || !['OWNER', 'ADMIN'].includes(currentUserRole))
+    return null;
+
   const res = await prisma.projectMember.updateMany({
     where: {
       projectId: selectedProject.id,
-      userId: memberId,
+      id: memberId,
     },
     data: {
+      role: role,
+    },
+  });
+
+  return res;
+};
+
+export const removeMember = async (memberId: string) => {
+  const selectedProject = await getSelectedProject();
+  if (!selectedProject) return null;
+
+  const currentUserRole = await getRoleOfCurrentUser();
+  if (!currentUserRole || !['OWNER', 'ADMIN'].includes(currentUserRole))
+    return null;
+
+  const res = await prisma.projectMember.deleteMany({
+    where: {
+      projectId: selectedProject.id,
+      id: memberId,
+      role: {
+        not: 'OWNER',
+      },
+    },
+  });
+
+  return res;
+};
+
+export const addProjectMember = async (name: string, role: string) => {
+  const selectedProject = await getSelectedProject();
+  if (!selectedProject) return null;
+
+  const currentUserRole = await getRoleOfCurrentUser();
+  if (!currentUserRole || !['OWNER', 'ADMIN'].includes(currentUserRole))
+    return null;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      name: name,
+    },
+  });
+
+  if (!user) throw new Error('User not found');
+  if (role === 'OWNER') throw new Error('Cannot add owner');
+
+  const res = await prisma.projectMember.create({
+    data: {
+      projectId: selectedProject.id,
+      userId: user.id,
       role: role,
     },
   });
--- src/features/members/utils.ts
+++ src/features/members/utils.ts
@@ -3,7 +3,8 @@ import { TMember } from './types';
 const roleColors = {
   OWNER: 'bg-purple-500 hover:bg-purple-400',
   ADMIN: 'bg-red-500 hover:bg-red-400',
-  DEVELOPPER: 'bg-green-500 hover:bg-green-400',
+  MEMBER: 'bg-green-500 hover:bg-green-400',
+  GUEST: 'bg-blue-500 hover:bg-blue-400',
 } as const;
 
 type RoleColors = typeof roleColors;
@@ -11,9 +12,9 @@ type Role = keyof RoleColors;
 
 export function getRoleBadgeColor(
   role: TMember['role']
-): RoleColors[Role] | 'blue-500 hover:bg-blue-600' {
+): RoleColors[Role] | 'blue-500 hover:bg-blue-400' {
   return (
     (roleColors as { [key: string]: RoleColors[Role] })[role] ||
-    'bg-blue-500 hover:bg-blue-600'
+    'bg-blue-500 hover:bg-blue-400'
   );
 }
