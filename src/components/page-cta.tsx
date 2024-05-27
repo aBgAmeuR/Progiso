06e464dca5630f2780e25f07f711052fbb783032
aBgAmeuR <a.josset@yahoo.com>
2024-05-27 23:55:10 +0200

    chore: update page cta with new text and links

--- src/components/page-cta.tsx
+++ src/components/page-cta.tsx
@@ -8,20 +8,19 @@ export const PageCta = () => {
     <div className="container pb-24">
       <div className="bg-accent/40 flex flex-col items-center gap-2 rounded-lg border p-6 text-center md:rounded-xl md:p-12">
         <h2 className="text-3xl font-semibold tracking-tight">
-          Lorem ipsum dolor sit amet
+          <Balancer>Ready to get started?</Balancer>
         </h2>
         <h3 className="text-muted-foreground">
           <Balancer>
-            Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua.
+            Sign up now and experience seamless project management with Progiso.
           </Balancer>
         </h3>
         <div className="not-prose mx-auto mt-4 flex items-center gap-2">
           <Button asChild>
-            <Link href="#">Get Started</Link>
+            <Link href="/projects">Get Started</Link>
           </Button>
           <Button variant="link" asChild>
-            <Link href="#">Learn More {'->'}</Link>
+            <Link href="/projects">Learn More {'->'}</Link>
           </Button>
         </div>
       </div>
